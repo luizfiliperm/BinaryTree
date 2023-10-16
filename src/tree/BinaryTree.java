348740d62e63a83172d4d15b4e06d102d1fa0505
luizfiliperm <luizfilipemr17@gmail.com>
2023-10-16 10:12:33 -0300

    Created BinaryTree Class, and insert method

---
+++ src/tree/BinaryTree.java
@@ -0,0 +1,42 @@
+package tree;
+
+public class BinaryTree {
+    private Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public void insert(int num) {
+        Node newNode = new Node(num);
+        insert(root, newNode);
+    }
+
+    private void insert(Node node, Node newNode) {
+        if (root == null) {
+            root = newNode;
+        } else {
+            if (newNode.getNum() < node.getNum()) {
+                if (node.getLeft() == null) {
+                    node.setLeft(newNode);
+                } else {
+                    insert(node.getLeft(), newNode);
+                }
+            }else if(newNode.getNum() > node.getNum()){
+                if (node.getRight() == null) {
+                    node.setRight(newNode);
+                } else {
+                    insert(node.getRight(), newNode);
+                }
+            }
+        }
+    }
+}
